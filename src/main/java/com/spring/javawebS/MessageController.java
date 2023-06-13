1c8945cabe1574777fc2efd59372aa86574e6781
hyejinw <1389001@naver.com>
2023-06-13 10:43:04 +0900

    20230613_1043

---
+++ src/main/java/com/spring/javawebS/MessageController.java
@@ -0,0 +1,53 @@
+package com.spring.javawebS;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MessageController {
+	
+	@RequestMapping(value = "/message/{msgFlag}", method = RequestMethod.GET)
+	public String listGet(@PathVariable String msgFlag,
+			@RequestParam(name="mid", defaultValue = "", required=false) String mid,
+			Model model) {
+		
+		if(msgFlag.equals("guestInputOk")) {
+			model.addAttribute("msg", "게시글이 등록되었습니다.");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("guestInputNo")) {
+			model.addAttribute("msg", "게시글이 등록 실패~~~");
+			model.addAttribute("url", "/guest/guestInput");
+		}
+		else if(msgFlag.equals("guestAdminOk")) {
+			model.addAttribute("msg", "관리자 인증 성공");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("guestAdminNo")) {
+			model.addAttribute("msg", "관리자 인증 실패~~~");
+			model.addAttribute("url", "/guest/adminLogin");
+		}
+		else if(msgFlag.equals("adminLogout")) {
+			model.addAttribute("msg", "관리자 로그아웃");
+			model.addAttribute("url", "/");
+		}
+		else if(msgFlag.equals("guestDeleteOk")) {
+			model.addAttribute("msg", "방명록의 글이 삭제 되었습니다.");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		else if(msgFlag.equals("guestDeleteNo")) {
+			model.addAttribute("msg", "방명록의 글이 삭제 실패~~~");
+			model.addAttribute("url", "/guest/guestList");
+		}
+		
+		
+		
+		return "include/message";
+	}
+	
+	
+}
